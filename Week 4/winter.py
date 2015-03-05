499c33d3a790ed2a304e740752a16423c5e4ac6e
Ani <ani.stoicheva@gmail.com>
2015-03-06 01:06:49 +0200

    Adding the winter.

---
+++ Week 4/winter.py
@@ -0,0 +1,22 @@
+def winter_is_coming(seasons):
+
+	# if len(seasons) < 5:
+	# 	return False
+
+	past_seasons = 0
+
+	for a in seasons:
+		if a == 'winter':
+			past_seasons = 0 # ako mina prez winter - zapo4vam otnovo da broq ot 0la
+
+		else:
+			past_seasons += 1
+
+	if past_seasons < 5:
+		return False
+	else:
+		return True
+
+print(winter_is_coming(["winter", "summer", "summer", "summer", "spring", "spring"]))
+print(winter_is_coming(["winter", "summer", "summer", "summer", "spring"]))
+print(winter_is_coming(["winter", "summer", "summer", "summer", "spring", "spring", "winter"]))
