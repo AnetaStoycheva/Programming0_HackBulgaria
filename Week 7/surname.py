169ac33620d9929bac5c0706a472f75eaaeb56e0
Ani <ani.stoicheva@gmail.com>
2015-03-26 15:55:35 +0200

    Fixing a typo at the end of return.

--- Week 7/surname.py
+++ Week 7/surname.py
@@ -1,6 +1,6 @@
 def taken_name(surname_husband, surname_wife):
 
-    return surname_wife[-(len(surname_husband) + 1): -2] in surname_husband:
+    return surname_wife[-(len(surname_husband) + 1): -2] in surname_husband
 
 print(taken_name("Petrov", "Petrova"))
 print(taken_name("Ivanov", "Tsvetanova"))
